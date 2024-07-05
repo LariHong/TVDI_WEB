7e0710047d982b8379c3d8f2a35afa3c849a3bc6
LariHong <gccbs05591job@gmail.com>
2024-07-05 16:57:16 +0800

    0705

--- 20240705/jq19-DOM-Traversing/myJS.js
+++ 20240705/jq19-DOM-Traversing/myJS.js
@@ -1,5 +1,8 @@
 $(function () {
     $(".AA").click(function () {
-        $(this).siblings().css({ "background-color": "#93C" });
+        // $(this).siblings().css({ "background-color": "#93C" });
+
+        //可以是標籤名稱 或者是 選擇器
+        $(this).nextUntil("p").css({ "background-color": "#93C" });
     });
 });
